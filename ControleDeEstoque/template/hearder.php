9ef741f5f3ffcaf991b46737e71b67998b1e2943
alexcia_maria <alexcia_maria@SNSOO1EP01VM144.saojose.sc.senai.br>
2015-11-17 10:08:06 -0200

    Criação do template.

--- ControleDeEstoque/template/hearder.php
+++ ControleDeEstoque/template/hearder.php
@@ -1,8 +1,9 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?></title>
+        <link type="text/css" href="/template/css/app.css" />
+    </head>
+    <body>
 
